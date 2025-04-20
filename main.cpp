77f08d7f6ccbcaabd7f99bf387187c721b50eff2
Dirk Zimmermann <me@dirkz.com>
2025-04-20 09:35:40 +0200

    "Parse" command line arguments

--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-	int result = std::system("git annex");
-	cout << "result " << result << "\n";
-
-	return 0;
+	if (argc != 2)
+	{
+		cerr << "Usage: " << argv[0] << " <dir>\n";
+		cerr << "  where <dir> is the git annex directory you want to upload\n\n";
+	}
+	
+	return -1;
 }
