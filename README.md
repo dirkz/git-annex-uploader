7d88885865c4b3346cadb9ba4373e89f91249beb
Dirk Zimmermann <me@dirkz.com>
2025-04-20 09:55:14 +0200

    Usage

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ in a space preserving manner.
 
 Set up with Visual Studio, CMake, WSL 2 (Ubuntu).
 
-Basically a `git annex copy <file> --to <backend>`,
+Basically a load of`git annex copy <file> --to <remote>`
+for every file encountered,
 with corresponding `git annex get` and `git annex drop` commands.
--- main.cpp
+++ main.cpp
@@ -5,10 +5,12 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
+    if (argc != 3)
     {
-        cerr << "Usage: " << argv[0] << " <dir>\n";
-        cerr << "  where <dir> is the git annex directory you want to upload\n\n";
+        cerr << "Usage:\n\n" << argv[0] << " <dir> <remote>\n";
+        cerr << "  where\n";
+        cerr << "    <dir>    is the git annex directory you want to upload\n";
+        cerr << "    <remote> is the git annex remote you want to upload to\n\n";
         return -1;
     }
 
