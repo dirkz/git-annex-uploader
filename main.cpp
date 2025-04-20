931bd7888d7d5c25eb6054799a5ad40a2f8ca429
Dirk Zimmermann <me@dirkz.com>
2025-04-20 09:43:34 +0200

    Output the directory

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
-﻿#include <iostream>
-#include <cstdlib>
+﻿#include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
-	{
-		cerr << "Usage: " << argv[0] << " <dir>\n";
-		cerr << "  where <dir> is the git annex directory you want to upload\n\n";
-	}
-	
-	return -1;
+    if (argc != 2)
+    {
+        cerr << "Usage: " << argv[0] << " <dir>\n";
+        cerr << "  where <dir> is the git annex directory you want to upload\n\n";
+        return -1;
+    }
+
+    string directory{argv[1]};
+    cout << "Processing " << directory << " ...\n";
+
+    return 0;
 }
