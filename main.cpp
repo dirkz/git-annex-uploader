85276cf8e38b848ad83ec43caaf9f8a34cd4f5f3
Dirk Zimmermann <me@dirkz.com>
2025-04-20 17:22:06 +0200

    Use std::

--- main.cpp
+++ main.cpp
@@ -5,24 +5,23 @@
 
 #include "FileStatusStore.h"
 
-using namespace std;
 namespace fs = std::filesystem;
 
 int main(int argc, char *argv[])
 {
     if (argc != 3)
     {
-        cerr << "Usage:\n\n" << argv[0] << " <dir> <remote>\n";
-        cerr << "  where\n";
-        cerr << "    <dir>    is the git annex directory you want to upload\n";
-        cerr << "    <remote> is the git annex remote you want to upload to\n\n";
+        std::cerr << "Usage:\n\n" << argv[0] << " <dir> <remote>\n";
+        std::cerr << "  where\n";
+        std::cerr << "    <dir>    is the git annex directory you want to upload\n";
+        std::cerr << "    <remote> is the git annex remote you want to upload to\n\n";
         return -1;
     }
 
-    string directory{argv[1]};
-    string remote{argv[2]};
-    cout << "Remote " << remote << "\n";
-    cout << "Processing " << directory << " ...\n";
+    std::string directory{argv[1]};
+    std::string remote{argv[2]};
+    std::cout << "Remote " << remote << "\n";
+    std::cout << "Processing " << directory << " ...\n";
 
     fs::path current = fs::current_path();
 
