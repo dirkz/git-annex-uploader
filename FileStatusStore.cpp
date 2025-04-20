c5abf2fe4a082b4c019f5442600eb738d409a3e5
Dirk Zimmermann <me@dirkz.com>
2025-04-20 12:15:32 +0200

    Some error handling

--- FileStatusStore.cpp
+++ FileStatusStore.cpp
@@ -1,9 +1,28 @@
 #include "FileStatusStore.h"
 
+#include <iostream>
+#include <stdexcept>
 #include <string>
 
 FileStatusStore::FileStatusStore(const std::filesystem::path &directory)
+    : m_sqlFile{directory / "git_annex_uploader.sql"}
 {
-    std::string sqlFile = directory / "git_annex_uploader.sql";
-    int result = sqlite3_open(sqlFile.c_str(), &m_sqlite);
+    std::cout << "SQL " << m_sqlFile << "\n";
+    int result = sqlite3_open(m_sqlFile.c_str(), &m_sqlite);
+    if (result != SQLITE_OK)
+    {
+    }
+}
+
+FileStatusStore::~FileStatusStore()
+{
+    if (m_sqlite)
+    {
+        int result = sqlite3_close(m_sqlite);
+        if (result != SQLITE_OK)
+        {
+            std::string errorMsg = "Could not close database: " + m_sqlFile;
+            std::cerr << errorMsg << "\n";
+        }
+    }
 };
--- FileStatusStore.h
+++ FileStatusStore.h
@@ -7,7 +7,9 @@
 struct FileStatusStore
 {
     FileStatusStore(const std::filesystem::path &directory);
+    ~FileStatusStore();
 
   private:
+    std::string m_sqlFile;
     sqlite3 *m_sqlite;
 };
